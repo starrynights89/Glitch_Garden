dae53af957935e8875a25a586f5cbd6585069271
Alexander Hartson <alexander-hartson@live.com>
2018-03-01 16:17:30 -0600

    code changes

---
+++ Number Wizard ui/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level load requested for: "+name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log("I want to quit!");
+	}
+}
