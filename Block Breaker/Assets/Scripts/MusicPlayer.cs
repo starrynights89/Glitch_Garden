8737000bf6bb603f0a8f2a0c9a796fc280df0679
Alexander Hartson <alexander-hartson@live.com>
2018-03-02 22:25:52 -0600

    code upload

--- Block Breaker/Assets/Scripts/MusicPlayer.cs
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start() {
+		GameObject.DontDestroyOnLoad(transform.root.gameObject);
 	}
 }
