3fbc1c488f280cd69616a50b807e2b216b845ef4
Alexander Hartson <alexander-hartson@live.com>
2018-03-07 12:52:05 -0600

    code changes

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
 	public Sprite[] hitSprites;
+	
 	private int timesHit;
-
 	private LevelManager levelManager;
 	// Use this for initialization
 	void Start () {
@@ -22,8 +21,18 @@ public class Brick : MonoBehaviour {
 	void OnCollisionEnter2D (Collision2D col)
 	{
 		timesHit++;
+		int maxHits = hitSprites.Length + 1;
 		if(timesHit >= maxHits) {
 			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 		}
 	}
 
