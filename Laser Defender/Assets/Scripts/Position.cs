bdb03cb1dc3660f02bc37e76b456b558e31dd414
Alexander Hartson <alexander-hartson@live.com>
2018-03-09 22:53:55 -0600

    file changes

---
+++ Laser Defender/Assets/Scripts/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour {
+
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
