a97062471e89ad2d885002d152e78bdbe22347fe
Alexander Hartson <alexander-hartson@live.com>
2018-03-09 23:28:50 -0600

    code changes

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -7,8 +7,11 @@ public class EnemySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity) as GameObject;
-		enemy.transform.parent = transform;
+		foreach( Transform child in transform ) {
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position,
+			Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
 	}
 	
 	// Update is called once per frame
