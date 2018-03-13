27ad1060382bb70c8a9d84531d1691611ee3a324
Alexander Hartson <alexander-hartson@live.com>
2018-03-12 20:22:45 -0500

    code changes

--- Laser Defender/Assets/Scripts/EnemyBehavior.cs
+++ Laser Defender/Assets/Scripts/EnemyBehavior.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour {
 
-	public float health = 150;
+	public GameObject projectile;
+	public float  projectileSpeed = 10f;
+	public float health = 150f;
+	public float shotsPerSeconds = 0.5f;
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		Projectile missile = collider.gameObject.GetComponent<Projectile>();
@@ -15,5 +18,19 @@ public class EnemyBehavior : MonoBehaviour {
 				Destroy(gameObject);
 			}
 		}
+	}
+
+	void Update() {
+		float probability = Time.deltaTime * shotsPerSeconds;
+		if(Random.value < probability) {
+			Fire();
+		}
+	}
+
+	void Fire() {
+		Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
+		GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity)
+		as GameObject;
+		missile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
 	}
 }
--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
 	public GameObject projectile;
 	public float projectileSpeed;
 	public float firingRate = 0.2f;
+	public float health = 250f;
 
 	float xmin;
 	float xmax;
@@ -22,7 +23,8 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void Fire() {
-		GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity);
+		Vector3 offset = new Vector3(0, 1, 0);
+		GameObject beam = Instantiate(projectile, transform.position+offset, Quaternion.identity);
 		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
 	}
 	// Update is called once per frame
@@ -43,5 +45,17 @@ public class PlayerController : MonoBehaviour {
 		// restrict the player to the gamespace
 		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
 		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile>();
+		if(missile){
+			Debug.Log("Player Collided with missle");
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
 	}
 }
