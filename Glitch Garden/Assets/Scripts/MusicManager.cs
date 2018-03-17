5aab4fb69b57c327e93155189d8a0185b78765a6
Alexander Hartson <alexander-hartson@live.com>
2018-03-17 00:40:38 -0500

    code upload

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    static MusicManager instance = null; //static instance of music player
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+    // Use this for initialization
+
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't destroy on load: " + name);
+	}
+	 void Start() {
+		audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		Debug.Log("Playing clip: " + levelMusicChangeArray);
+	}
+}
