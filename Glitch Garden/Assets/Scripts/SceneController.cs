62bebdd160c85f323c0e17e25b5340f36149c41d
Alexander Hartson <alexander-hartson@live.com>
2018-03-16 19:26:21 -0500

    changes

---
+++ Glitch Garden/Assets/Scripts/SceneController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public float autoLoadNextLevel;
+
+    /// <summary>
+    /// Initialisation
+    /// </summary>
+    void Start()
+    {
+        if (autoLoadNextLevel > 0) {
+            Invoke("LoadNextLevel", autoLoadNextLevel);
+        }
+    }
+
+    /// <summary>
+    /// Loads a scene for the specified name
+    /// </summary>
+    /// <param name="name">The name of the scene to load</param>
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Scene load: " + name);
+        Scene s = SceneManager.GetSceneByName(name);
+        Debug.Log("Scene is:");
+        Debug.Log(s.name);
+        Debug.Log(s.buildIndex);
+        SceneManager.LoadScene(name);
+    }
+
+    /// <summary>
+    /// Loads the next scene in order of build index
+    /// </summary>
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Handles a request to quit the game
+    /// </summary>
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+}
