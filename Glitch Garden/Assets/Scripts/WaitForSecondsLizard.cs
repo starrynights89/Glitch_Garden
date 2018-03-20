1bec10cb0c7cec0f358976fd6e31bed0d25c0d5b
Alexander Hartson <alexander-hartson@live.com>
2018-03-19 22:52:03 -0500

    code changes

---
+++ Glitch Garden/Assets/Scripts/WaitForSecondsLizard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForSecondsLizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Appearing());
+	}
+
+	IEnumerator Appearing() {
+		print(Time.time);
+		yield return new WaitForSeconds(1);
+		print(Time.time);
+	}
+}
