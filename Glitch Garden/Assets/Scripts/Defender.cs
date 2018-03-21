56fe4ca29a5215e4d52c879b15f29d6ccb26aa5e
Alexander Hartson <alexander-hartson@live.com>
2018-03-21 13:21:31 -0500

    commits

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attacker : MonoBehaviour {
-
-	[Range (-1f, 1.5f)]
-	public float walkSpeed;
+public class Defender : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
-		myRigidbody.isKinematic = true;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+		
 	}
 
 	void OnTriggerEnter2D() {
