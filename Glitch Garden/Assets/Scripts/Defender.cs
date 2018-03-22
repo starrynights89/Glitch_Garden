0ae10ccb7035f444dcfa4bc238b4db898ed2e0ce
Alexander Hartson <alexander-hartson@live.com>
2018-03-22 11:40:31 -0500

    commits

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	//Only being used as a tag for now!
 
-	void OnTriggerEnter2D() {
-		Debug.Log(name + " trigger enter");
-	}
 }
--- Glitch Garden/Assets/Scripts/Stone.cs
+++ Glitch Garden/Assets/Scripts/Stone.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	//Only being used as a tag for now!
 }
