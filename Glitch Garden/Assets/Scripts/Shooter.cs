fffc0db35d30891c9ceb1663552c28f9d1dbe00e
Alexander Hartson <alexander-hartson@live.com>
2018-03-22 19:20:38 -0500

    commits

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public GameObject projectile, projectileParent, gun;
+
+	private void Fire() {
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.parent = projectileParent.transform;
+		newProjectile.transform.position = gun.transform.position;
 	}
 }
