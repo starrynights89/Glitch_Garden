eb264f6f667f60b6a9ab836d485b0229e96d4d87
Alexander Hartson <alexander-hartson@live.com>
2018-03-22 21:08:11 -0500

    commits

--- Glitch Garden/Assets/Scripts/Projectile.cs
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -15,4 +15,14 @@ public class Projectile : MonoBehaviour {
 	void Update () {
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Attacker attacker = collider.gameObject.GetComponent<Attacker>();
+		Health health = collider.gameObject.GetComponent<Health>();
+
+		if (attacker && health) {
+			health.DealDamage(damage);
+			Destroy(gameObject);
+		}
+	}
 }
