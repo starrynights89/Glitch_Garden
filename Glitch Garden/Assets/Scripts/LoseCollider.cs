70df5f48e3521afe498c31757073a27bfc2c265b
Alexander Hartson <alexander-hartson@live.com>
2018-03-23 23:03:30 -0500

    commits

---
+++ Glitch Garden/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	private SceneController LevelManager; 
+
+	void OnTriggerEnter2D() {
+		LevelManager = GameObject.FindObjectOfType<SceneController>();
+		LevelManager.LoadLevel("03b Lose");
+	}
+}
